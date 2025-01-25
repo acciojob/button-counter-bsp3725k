3dcf2405223eea15f9f99e05caff6b08007f16dd
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-25 18:57:26 +0530

    Button clicks

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  const [num, setNum] = useState(0);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {num} times</p>
+        <button onClick={()=>setNum(num+1)}>Click me</button>
     </div>
   )
 }
